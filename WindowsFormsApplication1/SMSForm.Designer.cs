8e03f62bfadeb5e848ddeacece8e8c57acb1a9f2
bhimbol <46390313+bhimbol@users.noreply.github.com>
2019-05-19 13:05:42 +0800

    Test

---
+++ WindowsFormsApplication1/SMSForm.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class SMSForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.smsTimer = new System.Windows.Forms.Timer(this.components);
+            this.cbPortnames = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(21, 248);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(222, 23);
+            this.btnSend.TabIndex = 0;
+            this.btnSend.Text = "button1";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Location = new System.Drawing.Point(21, 96);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(222, 20);
+            this.txtNumber.TabIndex = 1;
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(21, 136);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(222, 106);
+            this.txtMessage.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Number";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Message";
+            // 
+            // txtLog
+            // 
+            this.txtLog.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.txtLog.Location = new System.Drawing.Point(260, 96);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtLog.Size = new System.Drawing.Size(416, 171);
+            this.txtLog.TabIndex = 5;
+            // 
+            // smsTimer
+            // 
+            this.smsTimer.Enabled = true;
+            this.smsTimer.Interval = 1000;
+            // 
+            // cbPortnames
+            // 
+            this.cbPortnames.FormattingEnabled = true;
+            this.cbPortnames.Location = new System.Drawing.Point(555, 25);
+            this.cbPortnames.Name = "cbPortnames";
+            this.cbPortnames.Size = new System.Drawing.Size(121, 21);
+            this.cbPortnames.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(552, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "List of PORTS/COM";
+            // 
+            // SMSForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(699, 279);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cbPortnames);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.txtNumber);
+            this.Controls.Add(this.btnSend);
+            this.Name = "SMSForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SMS";
+            this.Load += new System.EventHandler(this.SMSForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.Timer smsTimer;
+        private System.Windows.Forms.ComboBox cbPortnames;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ WindowsFormsApplication1/SMSForm.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+namespace WindowsFormsApplication1
+{
+    public partial class SMSForm : Form
+    {
+        public AutoResetEvent receiveNow;
+        public SerialPort smsPort = new SerialPort();
+
+        public SMSForm()
+        {
+            InitializeComponent();
+        }
+
+        private void SMSForm_Load(object sender, EventArgs e)
+        {
+            foreach(var p in SerialPort.GetPortNames())
+            {
+                cbPortnames.Items.Add(p.ToString());
+            }
+        }
+
+        public SerialPort OpenPort(string portName, int baudRate, int dataBits, int readTimeout, int writeTimeout)
+        {
+            receiveNow = new AutoResetEvent(false);
+            SerialPort port = new SerialPort();
+            try
+            {
+                port.PortName = portName;                 //COM1
+                port.BaudRate = baudRate;                 //9600
+                port.DataBits = dataBits;                 //8
+                port.StopBits = StopBits.One;             //1
+                port.Parity = Parity.None;                //None
+                port.ReadTimeout = readTimeout;           //300
+                port.WriteTimeout = writeTimeout;         //300
+                port.Encoding = Encoding.GetEncoding("iso-8859-1");
+                port.DataReceived += new SerialDataReceivedEventHandler(port_DataReceived);
+                port.Open();
+                port.DtrEnable = true;
+                port.RtsEnable = true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return port;
+        }
+
+        public void port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                if (e.EventType == SerialData.Chars)
+                {
+                    receiveNow.Set();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void ClosePort(SerialPort port)
+        {
+            try
+            {
+                port.Close();
+                port.DataReceived -= new SerialDataReceivedEventHandler(port_DataReceived);
+                port = null;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public string SendATCommand(SerialPort port, string command, int responseTimeout, string errorMessage)
+        {
+            try
+            {
+                port.DiscardOutBuffer();
+                port.DiscardInBuffer();
+                receiveNow.Reset();
+                port.Write(command + "\r");
+
+                string input = ReadResponse(port, responseTimeout);
+                if ((input.Length == 0) || ((!input.EndsWith("\r\n> "))
+                            && (!input.EndsWith("\r\nOK\r\n"))))
+                    throw new ApplicationException("No success message was received.");
+                return input;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public string ReadResponse(SerialPort port, int timeout)
+        {
+            string serialPortData = string.Empty;
+            try
+            {
+                do
+                {
+                    if (receiveNow.WaitOne(timeout, false))
+                    {
+                        string data = port.ReadExisting();
+                        serialPortData += data;
+                    }
+                    else
+                    {
+                        if (serialPortData.Length > 0)
+                            throw new ApplicationException("Response received is incomplete.");
+                        else
+                            throw new ApplicationException("No data received from phone.");
+                    }
+                }
+                while (!serialPortData.EndsWith("\r\nOK\r\n") &&
+                 !serialPortData.EndsWith("\r\n> ") && !serialPortData.EndsWith("\r\nERROR\r\n"));
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return serialPortData;
+        }
+
+        public bool SendMessage(SerialPort port, string phoneNo, string message)
+        {
+            bool isSend = false;
+            try
+            {
+                string recievedData = SendATCommand(port, "AT", 300, "No phone connected");
+                string command = "AT+CMGF=1" + char.ConvertFromUtf32(13);
+                recievedData = SendATCommand(port, command, 300, "Failed to set message format.");
+
+                command = "AT+CMGS=\"" + phoneNo + "\"" + char.ConvertFromUtf32(13);
+                recievedData = SendATCommand(port, command, 300,
+                    "Failed to accept phoneNo");
+
+                command = message + char.ConvertFromUtf32(26);
+                recievedData = SendATCommand(port, command, 3000,
+                    "Failed to send message"); //3 seconds
+
+                if (recievedData.EndsWith("\r\nOK\r\n"))
+                    isSend = true;
+                else if (recievedData.Contains("ERROR"))
+                    isSend = false;
+
+                return isSend;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        /*
+        public ShortMessageCollection ReadSMS(SerialPort port, string atCommand)
+        {
+            // Set up the phone and read the messages
+            ShortMessageCollection messages = null;
+            try
+            {
+
+                #region Execute Command
+                // Check connection
+                SendATCommand(port, "AT", 300, "No phone connected");
+                // Use message format "Text mode"
+                SendATCommand(port, "AT+CMGF=1", 300, "Failed to set message format.");
+                // Read the messages
+                string input = SendATCommand(port, atCommand, 5000, "Failed to read the messages.");
+                #endregion
+
+                #region Parse messages
+                messages = ParseMessages(input);
+                #endregion
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            if (messages != null)
+                return messages;
+            else
+                return null;
+        }
+        public ShortMessageCollection ParseMessages(string input)
+        {
+            ShortMessageCollection messages = new ShortMessageCollection();
+            try
+            {
+                Regex r = new Regex(@"\+CMGL: (\d+),""(.+)"",""(.+)"",(.*),""(.+)""\r\n(.+)\r\n");
+                Match m = r.Match(input);
+                while (m.Success)
+                {
+                    ShortMessage msg = new ShortMessage();
+                    msg.Index = m.Groups[1].Value;
+                    msg.Status = m.Groups[2].Value;
+                    msg.Sender = m.Groups[3].Value;
+                    msg.Alphabet = m.Groups[4].Value;
+                    msg.Sent = m.Groups[5].Value;
+                    msg.Message = m.Groups[6].Value;
+                    messages.Add(msg);
+                    m = m.NextMatch();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return messages;
+        }
+        */
+
+        public bool DeleteMessage(SerialPort port, string atCommand)
+        {
+            bool isDeleted = false;
+            try
+            {
+                #region Execute Command
+                string recievedData = SendATCommand(port, "AT", 300, "No phone connected");
+                recievedData = SendATCommand(port, "AT+CMGF=1", 300, "Failed to set message format.");
+                String command = atCommand;
+                recievedData = SendATCommand(port, command, 300, "Failed to delete message");
+                #endregion
+
+                if (recievedData.EndsWith("\r\nOK\r\n"))
+                {
+                    isDeleted = true;
+                }
+                if (recievedData.Contains("ERROR"))
+                {
+                    isDeleted = false;
+                }
+                return isDeleted;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            if(cbPortnames.Text != "")
+            {
+                if (smsPort.IsOpen == false)
+                {
+                    smsPort = OpenPort(cbPortnames.Text, 9600, 8, 300, 300);
+                }
+                //var sss = SendATCommand(smsPort, txtMessage.Text, 300, "happyMe");
+                //txtLog.Text = sss;
+                SendMessage(smsPort, txtNumber.Text, txtMessage.Text);
+            }
+            else
+            {
+                MessageBox.Show("Set your GSM PORT");
+            }
+        }
+    }
+}
